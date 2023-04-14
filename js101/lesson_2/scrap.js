95e5ed48bbf07a3db34b960eb15bd33d6efc97e8
Arno Cai <acai367@gmail.com>
2023-04-13 22:48:54 -0400

    File to test features/code. General purpose

---
+++ js101/lesson_2/scrap.js
@@ -0,0 +1,30 @@
+// A file to practice JS questions related to lesson 2. 
+
+
+// PSEUDOCODE 
+// a function that returns the sum of two numbers 
+/*
+START 
+
+Given two numbers, num1 and num2
+
+SET result = num1 + num2
+PRINT result
+
+END 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
