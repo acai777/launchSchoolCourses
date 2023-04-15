03e4e22121ed833fb931338e6575ad53b841a96e
Arno Cai <acai367@gmail.com>
2023-04-15 18:13:33 -0400

    Allow calculator program to ask user if they want to continue

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -16,44 +16,58 @@ function invalidNumber(number) {
 
 // Program start
 prompt("Welcome to the Calulator!");
-prompt("What is the first number?");
-let first = readline.question();
-while (invalidNumber(first) || first.trimStart() === '') {
-  prompt("Hmm...that doesn't look like a valid number.");
-  first = readline.question();
+let cont = true;
+
+while (cont) {
+  prompt("What is the first number?");
+  let first = readline.question();
+  while (invalidNumber(first) || first.trimStart() === '') {
+    prompt("Hmm...that doesn't look like a valid number.");
+    first = readline.question();
+  }
+
+
+  prompt("What is the second number? ");
+  let second = readline.question();
+  while (invalidNumber(second) || second.trimStart() === '') {
+    prompt("Hmm...that doesn't look like a valid number.");
+    second = readline.question();
+  }
+
+  prompt("What operation would you like to perform? \n1) Add 2) Subtract 3) Multiply 4) Divide ");
+  let operation = readline.question(); // is a string type
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt("Hmm...that doesn't look like a valid operation.\n=> Must choose 1, 2, 3, or 4.");
+    operation = readline.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(first) + Number(second); // need to convert to number types.
+      break;
+    case '2':
+      output = Number(first) - Number(second);
+      break;
+    case '3':
+      output = Number(first) * Number(second);
+      break;
+    case '4':
+      output = Number(first) / Number(second);
+      break;
+  }
+
+  prompt(`The result is ${output}`);
+  prompt("Would you like to perform another calculation (y/n)?");
+  let response = readline.question();
+
+  while (response.toLowerCase() !== 'y' && response.toLowerCase() !== 'n') {
+    prompt("Hmm...that is not a valid response. Please select either 'y' or 'n' and hit enter.");
+    response = readline.question();
+  }
+
+  if (response.toLowerCase() === 'n') {
+    cont = false;
+  }
 }
-
-
-prompt("What is the second number? ");
-let second = readline.question();
-while (invalidNumber(second) || second.trimStart() === '') {
-  prompt("Hmm...that doesn't look like a valid number.");
-  second = readline.question();
-}
-
-prompt("What operation would you like to perform? \n1) Add 2) Subtract 3) Multiply 4) Divide ");
-let operation = readline.question(); // is a string type
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("Hmm...that doesn't look like a valid operation.\n=> Must choose 1, 2, 3, or 4.");
-  operation = readline.question();
-}
-
-let output;
-switch (operation) {
-  case '1':
-    output = Number(first) + Number(second); // need to convert to number types.
-    break;
-  case '2':
-    output = Number(first) - Number(second);
-    break;
-  case '3':
-    output = Number(first) * Number(second);
-    break;
-  case '4':
-    output = Number(first) / Number(second);
-    break;
-}
-
-prompt(`The result is ${output}`);
-
 
