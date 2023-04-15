3d471c96afd8abf57c7a4b0f3e8d6d7899f13f5e
Arno Cai <acai367@gmail.com>
2023-04-15 12:51:16 -0400

    Refactored calculator. Added in user-input validation functions

--- js101/lesson_2/calculator.js
+++ js101/lesson_2/calculator.js
@@ -5,30 +5,55 @@
 // display the result of the operation
 
 const readline = require('readline-sync');
-console.log("Welcome to the Calulator!");
 
-console.log("What is the first number?");
-let first = readline.question();
-
-console.log("What is the second number? ");
-let second = readline.question();
-
-console.log("What operation would you like to perform? \n1) Add 2) Subtract 3) Multiply 4) Divide ");
-let operation = readline.question(); // is a string type
-
-//console.log(`${first}, ${second}`);
-
-let output;
-if (operation === '1') {
-  output = Number(first) + Number(second); // need to convert to number types.
-} else if (operation === '2') {
-  output = Number(first) - Number(second);
-} else if (operation === '3') {
-  output = Number(first) * Number(second);
-} else if (operation === '4') {
-  output = Number(first) / Number(second);
+function prompt(message) {
+  console.log(`=> ${message}`);
 }
 
-console.log(`The result is ${output}`);
+function invalidNumber(number) {
+  return Number.isNaN(Number(number));
+}
+
+// Program start
+prompt("Welcome to the Calulator!");
+prompt("What is the first number?");
+let first = readline.question();
+while (invalidNumber(first) || first.trimStart() === '') {
+  prompt("Hmm...that doesn't look like a valid number.");
+  first = readline.question();
+}
+
+
+prompt("What is the second number? ");
+let second = readline.question();
+while (invalidNumber(second) || second.trimStart() === '') {
+  prompt("Hmm...that doesn't look like a valid number.");
+  second = readline.question();
+}
+
+prompt("What operation would you like to perform? \n1) Add 2) Subtract 3) Multiply 4) Divide ");
+let operation = readline.question(); // is a string type
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt("Hmm...that doesn't look like a valid operation.\n=> Must choose 1, 2, 3, or 4.");
+  operation = readline.question();
+}
+
+let output;
+switch (operation) {
+  case '1':
+    output = Number(first) + Number(second); // need to convert to number types.
+    break;
+  case '2':
+    output = Number(first) - Number(second);
+    break;
+  case '3':
+    output = Number(first) * Number(second);
+    break;
+  case '4':
+    output = Number(first) / Number(second);
+    break;
+}
+
+prompt(`The result is ${output}`);
 
 
