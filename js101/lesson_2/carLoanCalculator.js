a62270732d58c5d3769d8974c580fadb3959daa1
Arno Cai <acai367@gmail.com>
2023-04-15 20:49:38 -0400

    Added rest of loan calculator helper functions

--- js101/lesson_2/carLoanCalculator.js
+++ js101/lesson_2/carLoanCalculator.js
@@ -5,13 +5,30 @@
 // Compute and return the monthly payment.
 
 let readline = require('readline-sync');
+function carLoanCalculator() {
 
+  // Loan amount
+  let loanAmount = getLoan();
+
+  // Annual percentage rate (APR)
+  let annualRate = getAPR(); 
+  let monthlyRate = (annualRate / 12) / 100; 
+
+  // Loan duration
+  let loanDuration = getDuration(); 
+
+  // Compute the monthly mortgage.  
+  let monthlyPaymt = loanAmount * (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDuration))));
+
+  console.log(`The monthly mortgage payment would be \$${monthlyPaymt.toFixed(2)}.`)
+}
+
+// Helper functions
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 function getLoan() {
-
   prompt("First, what is the loan amount? ");
   let loanAmount = readline.question().trimStart(); 
   if (loanAmount[0] == '$') {   // Note: allow for users to input "$" if desired.
@@ -44,17 +61,71 @@ function getLoan() {
   }
 }
 
-function carLoanCalculator() {
+function getAPR() {
+  prompt("What is your annual perentage rate? Please enter a number between 0 and 100 (%).");
+  let rate = readline.question(); 
+  if (rate[rate.length-1] === '%') {
+    rate = rate.slice(0,rate.length-1);
+  }
+  rate = parseFloat(rate); // convert to Number type 
 
-  // LOAN AMOUNT
-  loanAmount = getLoan();
+  // Verify is a valid rate
+  if (rate < 0 || rate >= 100 || Number.isNaN(rate)) {
+    prompt("You need to enter a number between 0 and 100. Please try again.\n");
+    return getAPR(); 
 
+  }
+  // Check with user the rate is what they inputted. 
+  while (true) {
+    prompt(`The annual percentage rate (APR) you've specified is ${rate}%. Is this correct (y/n)?`);
+    let response = readline.question();
+    
+    while (response.toLowerCase() !== 'y' && response.toLowerCase() !== 'n') {
+      prompt("Hmm...that is not a valid response. Please select either 'y' or 'n' and hit enter.");
+      response = readline.question();
+    }
 
-
+    if (response.toLowerCase() === 'n') {
+      prompt("Okay. Let's try again\n");
+      return getAPR(); // prompts the user to restart the process. Recursive callback.
+    } else {
+      return rate; 
+    }
+  }
 }
 
+function getDuration() {
+  prompt("What is the loan duration in months? Please enter an integer.");
+  let loanDuration = readline.question(); 
+
+  while ((Number.isNaN(parseInt(loanDuration)))) {
+    prompt("Please enter a valid duration.");
+    loanDuration = readline.question(); 
+  }
+
+  // Verify with user the input is correct. 
+  while (true) {
+    prompt(`The loan duration is: ${loanDuration} months. Is this correct (y/n)?`);
+    let response = readline.question();
+    
+    while (response.toLowerCase() !== 'y' && response.toLowerCase() !== 'n') {
+      prompt("Hmm...that is not a valid response. Please select either 'y' or 'n' and hit enter.");
+      response = readline.question();
+    }
+
+    if (response.toLowerCase() === 'n') {
+      prompt("No worries. Let's restart the process.\n");
+      return getDuration(); // prompts the user to restart the process. Recursive callback.
+    } else {
+      return loanDuration; 
+    }
+  }
+
+
+  
+  return loanDuration; 
+}
+
+// Run the script 
 prompt("Welcome to your personal car loan calculator.\n\n=> To calculate your monthly payment, we need you to answer a few questions. Here they are:\n");
 carLoanCalculator();
-
-
-
