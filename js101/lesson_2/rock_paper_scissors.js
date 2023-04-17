1c23a57e23c07615085208af0c6561b18ef028dc
Arno Cai <acai367@gmail.com>
2023-04-17 19:22:50 -0400

    create helper function to display winner and encapsulate

--- js101/lesson_2/rock_paper_scissors.js
+++ js101/lesson_2/rock_paper_scissors.js
@@ -16,30 +16,31 @@ while (true) {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  prompt(`You choice ${choice}, the computer chose ${computerChoice}.`);
+  displayWinner(choice, computerChoice);
 
+  prompt(`Would you like to play again? (y/n)`);
+  let answer = readline.question().toLowerCase(); 
+  while (answer !== 'y' && answer !== 'n') {
+    prompt(`Please enter a valid response; please enter 'y' or 'n'`);
+    answer = readline.question().toLowerCase(); 
+  }
+
+  if (answer !== 'y') break;
+}
+
+
+function displayWinner(choice, computerChoice) {
+  prompt(`You choice ${choice}, the computer chose ${computerChoice}.`);
   if ((choice === 'rock' && computerChoice === 'scissors') ||
       (choice === 'scissors' && computerChoice === 'paper') ||
       (choice === 'paper' && computerChoice === 'rock')) {
-        prompt(`You won!`);
-      } else if ((choice === 'scissors' && computerChoice === 'rock') ||
-          (choice === 'paper' && computerChoice === 'scissors') ||
-          (choice === 'rock' && computerChoice === 'paper')) {
-            prompt(`Computer won!`);
-      } else {
-        prompt(`It's a tie!`);
-      }
-
-      prompt(`Would you like to play again? (y/n)`);
-      let answer = readline.question().toLowerCase(); 
-
-      while (answer !== 'y' && answer !== 'n') {
-        prompt(`Please enter a valid response; please enter 'y' or 'n'`);
-        answer = readline.question().toLowerCase(); 
-      }
-
-      if (answer !== 'y') break;
+    prompt(`You won!`);
+  } else if ((choice === 'scissors' && computerChoice === 'rock') ||
+      (choice === 'paper' && computerChoice === 'scissors') ||
+      (choice === 'rock' && computerChoice === 'paper')) {
+        prompt(`Computer won!`);
+  } else {
+    prompt(`It's a tie!`);
   }
-
-
+}
 
