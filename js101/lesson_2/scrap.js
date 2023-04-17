084a562db61e5a2a06ca36d28fb73ef8f16a5706
Arno Cai <acai367@gmail.com>
2023-04-17 19:19:02 -0400

    add scope practice qs to scrap.js

--- js101/lesson_2/scrap.js
+++ js101/lesson_2/scrap.js
@@ -14,12 +14,31 @@ PRINT result
 END 
 */
 
+// function addName(arr, name) {
+//   arr = arr.concat([name]);
+//   console.log(arr)
+// }
+
+// let names = ["bob", "kim"];
+// addName(names, "jim");
+// console.log(names); // => [ 'bob', 'kim', ]
 
 
+function capitalize() {
+  return word[0].toUpperCase() + word.slice(1);
+}
 
+function exclaim() {
+  return word += '!!!';
+}
 
+let word = 'hello';
+let capitalizedWord = capitalize(word);
+let exclaimedWord = exclaim(capitalizedWord);
 
-
+console.log(word);
+console.log(capitalizedWord);
+console.log(exclaimedWord);
 
 
 
