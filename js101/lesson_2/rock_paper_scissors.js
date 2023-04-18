887dc5a1b33192111104f84416b1cb734aa1fb92
Arno Cai <acai367@gmail.com>
2023-04-17 21:13:47 -0400

    Added a welcome msg displaying all rules

--- js101/lesson_2/rock_paper_scissors.js
+++ js101/lesson_2/rock_paper_scissors.js
@@ -1,10 +1,24 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
+const RULES = [
+  'Scissors cut paper',
+  'Paper covers rock',
+  'Rock crushes lizard',
+  'Lizard poisons Spock',
+  'Spock smashes (or melts) scissors',
+  'Scissors decapitate lizard',
+  'Lizard eats paper',
+  'Paper disproves Spock',
+  'Spock vaporizes rock',
+  'Rock breaks scissors',
+]
+const NUMBER_RULES = 10; 
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+welcomeMsg();
 while (true) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
   let choice = readline.question(); 
@@ -28,7 +42,6 @@ while (true) {
   if (answer !== 'y') break;
 }
 
-
 function displayWinner(choice, computerChoice) {
   prompt(`You choice ${choice}, the computer chose ${computerChoice}.`);
   if ((choice === 'scissors' && computerChoice === 'paper') ||
@@ -40,7 +53,7 @@ function displayWinner(choice, computerChoice) {
       (choice === 'paper' && computerChoice === 'spock') ||
       (choice === 'rock' && computerChoice === 'scissors') ||
       (choice === 'lizard' && computerChoice === 'paper') ||
-      (choice === 'spock' && computerChoice === 'paper')) {
+      (choice === 'spock' && computerChoice === 'rock')) {
     prompt(`You won!`);
   } else if ((computerChoice === 'scissors' && choice === 'paper') ||
       (computerChoice === 'paper' && choice === 'rock') ||
@@ -51,10 +64,24 @@ function displayWinner(choice, computerChoice) {
       (computerChoice === 'paper' && choice === 'spock') ||
       (computerChoice === 'rock' && choice === 'scissors') ||
       (computerChoice === 'lizard' && choice === 'paper') ||
-      (computerChoice === 'spock' && choice === 'paper')) {
+      (computerChoice === 'spock' && choice === 'rock')) {
         prompt(`Computer won!`);
   } else {
     prompt(`It's a tie!`);
   }
 }
+
+function welcomeMsg() {
+  console.clear();
+
+  prompt(`Welcome to Rock Paper Scissors Lizard Spock!`)
+  prompt(`This is a game where you must play against the computer in a best of five game`);
+  prompt(`You might be wondering which gesture beats what. Here are the possibilities:\n`)
+  RULES.forEach(rule => console.log(`     ${rule}`));
+  
+  console.log(); 
+  prompt(`Let's get started.`);
+}
+
+
 
