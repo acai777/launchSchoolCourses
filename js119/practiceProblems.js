e6ff7cae86b9ffd2ca4e38dab225f3d862a04376
Arno Cai <acai367@gmail.com>
2023-05-19 20:48:14 -0400

    created js file for js 119 practice problems

---
+++ js119/practiceProblems.js
@@ -0,0 +1,26 @@
+// This file contains code for the practice problems for the JS 119 interview 
+//////////////////////////////////////////////////////////////////////////////
+
+// Given an array of numbers, for each number, find out how
+// many numbers in the array are smaller than it. When
+// counting numbers, only count unique values. That is, if a
+// given number occurs multiple times in the array, it
+// should only be counted once.
+
+// Examples:
+
+console.log(smallerNumbersThanCurrent([8, 1, 2, 2, 3])); // [3, 0, 1, 1, 2]
+console.log(smallerNumbersThanCurrent(
+  [1, 4, 6, 8, 13, 2, 4, 5, 4])); // [0, 2, 4, 5, 6, 1, 2, 3, 2]
+console.log(smallerNumbersThanCurrent([7, 7, 7, 7])); // [0,0,0,0]
+console.log(smallerNumbersThanCurrent([6, 5, 4, 8])); // [2, 1, 0, 3]
+console.log(smallerNumbersThanCurrent([1])); // [0]
+
+
+
+
+
+
+
+
+
