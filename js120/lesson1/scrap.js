0c24e2ed62bb410ece9eccbec64351afb8da1723
Arno Cai <acai367@gmail.com>
2023-05-24 13:49:09 -0400

    Completed object factory function exercise on books

--- js120/lesson1/scrap.js
+++ js120/lesson1/scrap.js
@@ -50,8 +50,34 @@ let raceCarOwn = createCar('Jaguar', 0.4, false);
 ////////////////////////////////////////////////
 // PRACTICE PROBLEMS
 ///////////////////////////////////////////////
+function createBook(title, author, read = false) {
+  let bookObj = {
+    title, 
+    author, 
+    read,
 
+    getDescription() {
+      let readStatus; 
+      if (this.read === true) readStatus = 'have';
+      if (this.read === false) readStatus = 'haven\'t';
+      return `${this.title} was written by ${this.author}. I ${readStatus} read it.`;
+    },
 
+    readBook() {
+      this.read = true;
+    }, 
+  };
+
+  return bookObj;
+}
+
+let book1 = createBook("Mythos", "Stephen Fry");
+let book2 = createBook("Me Talk Pretty One Day", "David Sedaris", false);
+let book3 = createBook("Aunts aren't Gentlemen", "PG Wodehouse", true);
+
+console.log(book1.getDescription()); 
+console.log(book2.getDescription());
+console.log(book3.getDescription());
 
 
 
