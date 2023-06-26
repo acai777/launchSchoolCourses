a3c6276d892351a78d08a76197d66375d59b87d9
Arno Cai <acai367@gmail.com>
2023-06-26 11:50:00 -0400

    Making files for triangle question

---
+++ javascriptChallenges/triangle.js
@@ -0,0 +1,11 @@
+/* Write a program to determine whether a triangle is equilateral, isosceles, or scalene.
+An equilateral triangle has all three sides the same length.
+An isosceles triangle has exactly two sides of the same length.
+A scalene triangle has all sides of different lengths.
+
+A triangle must also satisfy two other conditions:
+1) all sides must be of length > 0
+2) the sum of lengths of any two side must be greater than the length of the third side
+
+
+*/
