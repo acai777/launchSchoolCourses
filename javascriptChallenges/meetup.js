3acbe864df6d0c3aaa0ad35e193a16c9a2bd8438
Arno Cai <acai367@gmail.com>
2023-06-29 15:12:42 -0400

    Finished meetup challenge problem

---
+++ javascriptChallenges/meetup.js
@@ -0,0 +1,127 @@
+/*
+2:25pm - 310pm. Took about 45min. This question was particularly difficult because you had to learn about the Date constructor and identify which methods were important while simulating a test environment. Solution is MUCH uglier than suggested one. If go back and have time, would refactor heavily and consider easier logic. Learned a lot about the Date constructor and its quirks.
+
+-class Meetup
+  -two inputs. First is number for year (e.g., 2013), second is number representing the month (number 1-12).
+-`day()` instance method. Takes in two arguments. 
+  -first arg = string representing day of week:'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', and 'Sunday'
+  -second arg = string representing descriptor: 'first', 'second', 'third', 'fourth', 'fifth', 'last', and 'teenth'. 
+  -NOTE: case unimportant. suggest converting and doing all in lower case. 
+-`toString` method. 
+
+thirteenth
+fourteenth 
+fifteenth
+sixteenth
+seventeenth
+eighteenth
+nineteenth
+
+Process:
+-`Meetup`. two number inputs. 
+  Write as separate properties of child instance i.e.,
+  this.month = month;
+  this.day = day;
+
+-`day` instance method. Takes in two strings `day` and `descriptor`. Lower case both. convert the `day` to numeric representation of day of week. Use for matching later. Returns date object. 
+  -now, check `descriptor'. do brute force process. if `descriptor' === 'teenth', run helper method `teenthDay`, which looks on the 12th through 19th day. Use Date constructor to check. 
+  -if `descriptor' === "last", start from the end until you see the right day. 
+  -otherwise, use counter and start from the beginning of month to see how many times you see the day. start counter = 0. Once you see the day, add to counter and see if matches. 
+
+///////////////////////////////////////////
+// trick is you need to get the dat to put into 
+
+//dictionary converting from string day to their number equivalent. Date.prototypegetDay(), where Sunday = 0, Saturday = 6. Can just check all the days in the month until you see the day you want. 
+//if descriptor in ['first', 'second', 'third', 'fourth', 'fifth'], go in order, do this way 
+// if descriptor === last, start from the end of the month and count down until the day is the day you specified. 
+// if descript === 'teenth' start from the 12th all the way up to the 19th until you see the day you want. 
+
+//TLDR: three dif methods for each type of descriptor.
+*/
+
+class Meetup {
+  constructor(year, month) {
+    this.year = year;
+    this.month = month - 1; 
+  }
+
+  day(dayOfWeek, descriptor) {
+    let toNumberMap = {
+      'sunday': 0,
+      'monday': 1,
+      'tuesday': 2,
+      'wednesday': 3, 
+      'thursday': 4,
+      'friday': 5, 
+      'saturday': 6,
+    };
+
+    dayOfWeek = dayOfWeek.toLowerCase(); 
+    let dayOfWeekNum = toNumberMap[dayOfWeek];
+    descriptor = descriptor.toLowerCase();
+
+    if (descriptor === 'teenth') {
+      return this.teenth(dayOfWeekNum);
+    } else if (descriptor === 'last') {
+      return this.last(dayOfWeekNum);
+    } else {
+      return this.inOrder(dayOfWeekNum, descriptor);
+    }
+  }
+
+
+  teenth(dayOfWeekNum) { // input is number 0-6
+    for (let index = 13; index <= 19; index +=1) {
+      let day = new Date(this.year, this.month, index).getDay(); 
+      if (day === dayOfWeekNum) {
+        return new Date(this.year, this.month, index);
+      }
+    }
+  }
+
+  last(dayOfWeekNum) {
+    let month = this.month + 1; 
+    for (let index = 0; index >= -31; index -=1) {
+      let day = new Date(this.year, month, index).getDay(); 
+      if (day === dayOfWeekNum) {
+        return new Date(this.year, month, index);
+      }
+    }
+  }
+
+  inOrder(dayOfWeekNum, descriptor) {
+    let toNumberOccurrences = {
+      'first': 1,
+      'second': 2,
+      'third': 3,
+      'fourth': 4,
+      'fifth': 5, 
+    }
+
+    let count = 0;
+    let numberOccurrences = toNumberOccurrences[descriptor];
+    for (let index = 1; index <= 31; index +=1) {
+      let day = new Date(this.year, this.month, index).getDay(); 
+      if (day === dayOfWeekNum) {
+        count +=1; 
+        if (count === numberOccurrences) {
+          return new Date(this.year, this.month, index);
+        }
+      } 
+    }
+
+    return null;
+  }
+}
+
+module.exports = Meetup;
+
+
+// function dateAsString(year, month, day) {
+//   let date = new Date(year, month - 1, day);
+//   console.log(date.toString());
+// }
+
+// dateAsString(2013, 4, 0); // gets you March 31, 2013
+//dateAsString(2013, 4, -1); // // gets you March 30, 2013
+
--- javascriptChallenges/scrap.js
+++ javascriptChallenges/scrap.js
@@ -1,72 +1,58 @@
-class Diamond {
-  static LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-  static getWidthMax(letter) {
-    // Special case if trying to find for letter input = 'A'
-    if (letter === 'A') {
-      return 0; 
-    }
-
-    let rowNumber = Diamond.LETTERS.indexOf(letter) + 1; 
-    let widthWidest = (2 * rowNumber) - 3; // max width between letters
-    let rowMaxWidth = widthWidest + 2; // max width of the row 
-
-    return rowMaxWidth; 
-  }
-
-  static letterRange(letter) {
-    let letterRange = [];
-    for (let index = 0; index < Diamond.LETTERS.length; index += 1) {
-      let currLetter = Diamond.LETTERS[index];
-      letterRange.push(currLetter);
-      if (currLetter === letter) break; 
-    }
-
-    return [...letterRange, ...letterRange.reverse().slice(1)];
-  }
-
-  static makeDiamond(letter) {
-    let result = [];
-    let maxWidth = Diamond.getWidthMax(letter);
-    let letterRange = Diamond.letterRange(letter);
-
-    for (let index = 0; index < letterRange.length; index +=1) {
-      let currLetter = letterRange[index];
-      result.push(Diamond.returnRow(currLetter, maxWidth));
-    }
-
-    return result.join("");
-  }
-
-  static returnRow(currLetter, maxWidth) {
-    let row = ' '.repeat(maxWidth).split("");
-    let currLetterIndex = Diamond.LETTERS.indexOf(currLetter);
-    let leftIndex = Math.floor(maxWidth / 2) - currLetterIndex; 
-    let rightIndex = Math.floor(maxWidth / 2) + currLetterIndex; 
-
-    row[leftIndex] = currLetter; 
-    row[rightIndex] = currLetter;  
-
-    return row.join("") + '\n';
-  }
-
-}
-
-module.exports = Diamond;
-
-console.log(Diamond);
-console.log(Object.getOwnPropertyNames(Diamond));
-
-Diamond.test = function() {
-  console.log(this);
-}
-
-Diamond.test();
-
-let myObj = {
-  myFcn() {
-    console.log(this);
+function delayLog() {
+  for (let delay = 1; delay <= 10; delay += 1) {
+    setTimeout(() => console.log(delay), 1000);
   }
 }
-myObj.myFcn();
+
+//delayLog();
+
+/*
+I believe this question tests our understanding of closure, and of how the event loop, in relation with the callback stack and task queue, work. 
+We first define the function `delayLog` on lines 1-5, and then invoke it on line 7. Once invoked, JS will then call each 
+`setTimeout` function. It will run through the entire for loop inside completely, finish executing each setTimeout invocation, and then pop `delayLog` off from the call stack (though, 
+due to the asynchronous nature of `setTimeout` the callback function inside won't actually be invoked until after the callstack is empty and the timer 
+finishes... more on that below). Note that, due to closure, because `delay` is declared via `let` here, it is block scope. This means that each 
+callback function inside the `setTimeout` function, when invoked, will invoke a DIFFERENT `delay` each time. 
+
+While the `setTimeout` function has finished running, what happens is each `setTimeout` call sets off a timer. That is, each `setTimeout` 
+invocation sets a timer on an API (according to the video we watched on the event loop, on Node, is some C++ API), and the API will handle the asynchronous nature. This is because `setTimeout` is an API provided to us by an external C++ API. 
+ 
+Once the timer finishes (which is dependent on the amount of time you told `setTimeout` to wait), the API will send the callback function to the task queue. Once your entire program finishes running (the call stack is empty), only then will the event loop grab the callback function from the task queue, pop it, and put it on the call stack to be invoked. The event loop basically looks at the call stack and the task queue, and will only move things from the task queue onto the call stack when the callstack is empty.
+
+The event loop waits until the call stack is before before moving anything from the task queue to the call stack. why something like setTimeout(cb, 0) only invokes the cb function at the end of  your program once the other sequential stuff finishes 
+*/
+
+function startCounting() {
+  let num = 1;
+  let id = setInterval(() => {
+    console.log(num);
+    num += 1;
+  }, num * 1000);
+
+  return id; 
+}
+
+//startCounting();
+
+function stopCounting(id) {
+  clearInterval(id);
+}
+
+/*
+Some terminology/notes from the event loop video:
+
+@ 11:15 the callback function inside the setTimeout function would be called an asynchronous callback. It won't be invoked and processed synchronously. Instead, we delay the callback invocation until a later specified point. 
+
+@11:50 event loop intro starts. I think you just have to focus from this point forward, and can get the right terminology down. 
+
+We can think of setTimeout as an API provided to us via the browser/C++ api. 
+
+
+Actually, just read the following link. Way more concise and helpful: 
+https://gist.github.com/gokulkrishh/d493ecdcb0e2dbb6fe5ad3e678dd9f89#:~:text=Executing%20setTimeout%20actually%20calls%20out,like%20this%20available%20in%20node.&text=setTimeout%20is%20then%20finished%20executing,amount%20of%20time%20(1000ms).
+
+@ 13:00 is pretty critical too. 
+*/
+
+
 
